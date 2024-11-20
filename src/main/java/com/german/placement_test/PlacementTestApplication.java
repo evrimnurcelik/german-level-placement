93eb5e3c230d2000db8760dc0fdcb28f67ef726f
Evrim Nur Celik <evrimnurcelik@gmail.com>
2024-11-20 22:17:38 +0300

    readme

--- src/main/java/com/german/placement_test/PlacementTestApplication.java
+++ src/main/java/com/german/placement_test/PlacementTestApplication.java
@@ -2,12 +2,13 @@ package com.german.placement_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.german.placement_test.model")
 public class PlacementTestApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PlacementTestApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PlacementTestApplication.class, args);
+    }
 }
+
